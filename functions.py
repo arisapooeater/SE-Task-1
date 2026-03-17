013df04f1a3559975799edd5f8412935f913b20a
arisapooeater <arisa.komatsu@education.nsw.gov.au>
2026-03-17 23:20:49 +1100

    feat: added user interaction log function
    created a function for making a pandas dataframe with all user interactions. it works but i need to add it to other functions properly so its specific (especially for filter_characters() )

--- functions.py
+++ functions.py
@@ -1,6 +1,9 @@
 import requests # Must import requests so we can use API
 import pandas as pd
-import numpy
+from datetime import datetime
+import time
+import os
+
 
 
 # API Base URL for all MLP characters
@@ -24,12 +27,12 @@ def search_character(name): #functioning
     for c in characters:
         if c["name"] == name.title():
             return { #change formatting later
-                "name": c["name"],
-                "sex": c["sex"],
-                "kind": c["kind"],
-                "occupation": c["occupation"],
-                "residence": c["residence"],
-                "image": c["image"][0] if c["image"] else None
+                "Name": c["name"],
+                "Sex": c["sex"],
+                "Kind": ", ".join(c["kind"]) if isinstance(c["kind"], list) else c["kind"],
+                "Occupation": c["occupation"],
+                "Residence": c["residence"],
+                "Image": c["image"][0] if c["image"] else None
             }
     print("Character not found!")
     return 
@@ -53,9 +56,10 @@ def filter_characters():
 | 2. Kind        |
  ________________""")
     
+    time.sleep(1)
     main_filter = input("""
 Filter by (1/2): """).strip()
-    
+    time.sleep(1)
     if main_filter == "1":
         print("""
  ___________________
@@ -66,11 +70,16 @@ Filter by (1/2): """).strip()
  ___________________""")
         sub_filter = input("""
 Filter by (1/2): """).strip()
+        time.sleep(1)
         if sub_filter == "1":
+            print("""
+FEMALE CHARACTERS""")
             for c in characters:
                 if c["sex"] == "Female":
                     print(c["name"])
         elif sub_filter == "2":
+            print("""
+MALE CHARACTERS""")
             for c in characters:
                 if c["sex"] == "Male":
                     print(c["name"])
@@ -87,28 +96,41 @@ Filter by (1/2): """).strip()
 | 4. Alicorn         |
 | 5. Other creatures |
  ____________________""")
+        time.sleep(1)
         sub_filter = input("""
 Filter by (1/2/3/4/5): """).strip()
+        time.sleep(1)
         if sub_filter == "1":
+            print("""
+PEGASUS CHARACTERS""")
             for c in characters:
                 if "Pegasus" in c["kind"]:
                     print(c["name"])
         elif sub_filter == "2":
+            print("""
+UNICORN CHARACTERS""")
             for c in characters:
                 if "Unicorn" in c["kind"]:
                     print(c["name"])
         elif sub_filter == "3":
+            print("""
+EARTH PONY CHARACTERS""")
             for c in characters:
                 if "Earth" in c["kind"]:
                     print(c["name"])
         elif sub_filter == "4":
+            print("""
+ALICORN CHARACTERS""")
             for c in characters:
                 if "Alicorn" in c["kind"]:
                     print(c["name"])
         elif sub_filter == "5":
+            print("""
+OTHER CREATURES""")
             for c in characters:
                 if "Pegasus" not in c["kind"] and "Unicorn" not in c["kind"] and "Earth" not in c["kind"] and "Alicorn" not in c["kind"]:
-                    print(f"{c['name']}: {c['kind']}")
+                    kind = ", ".join(c["kind"]) if isinstance(c["kind"], list) else c["kind"]
+                    print(f"{c['name']}: {kind}")
         else:
             print("Invalid input. Enter a number between 1-5 (1/2/3/4/5). Returning to main menu...")
     else:
@@ -126,13 +148,12 @@ FAVOURITES LIST""")
             for name, details in favlist.items():
                 empty = False
                 print(f"""
-Name: {details['name']}
-Sex: {details['sex']}
-Kind: {details['kind']}
-Occupation: {details['occupation']}
-Residence: {details['residence']}
-Image: {details['image']}
-""") 
+Name: {details['Name']}
+Sex: {details['Sex']}
+Kind: {details['Kind']}
+Occupation: {details['Occupation']}
+Residence: {details['Residence']}
+Image: {details['Image']}""")
         else:
             empty = True
             print("""
@@ -142,7 +163,7 @@ FAVOURITES LIST
 (empty)""")
         print("""
 _____________________________________________________""")
-
+        time.sleep(1)
         print("""
  _________________________
 |         OPTIONS         |
@@ -151,9 +172,10 @@ _____________________________________________________""")
 | 2. Remove character     |
 | 3. Exit Favourites List |
  _________________________""")
+        time.sleep(1)
         user_input = input("""
 Enter action (1/2/3): """).strip()
-    
+        time.sleep(1)
         if user_input == "1":
             add_character()
         elif user_input == "2":
@@ -161,12 +183,15 @@ Enter action (1/2/3): """).strip()
                 remove_character()
             else:
                 print("Favourites List is empty. Action cannot be taken. Returning to Favourites List...")
-        else:
+        elif user_input == "3":
             print("""
  ____________________________
 | Exiting Favourites List... |
  ____________________________""")
             exit = True
+        else:
+            print("Invalid input. Enter a number between 1-3 (1/2/3). Returning to Favourites List...")
+        time.sleep(2)
 
 
 def add_character(): 
@@ -174,10 +199,12 @@ def add_character():
     name = input("""
 What character would you like to add? (enter full name): """).title().strip()
     character = search_character(name) 
+    time.sleep(1)
     if name in favlist:
         print("Character already in Favourites List! Returning to Favourites List...")
     elif character:
-        favlist[character["name"]] = character
+        log_df.loc[len(log_df)] = ["add character", name]
+        favlist[character["Name"]] = character
         print(f"""
  _____________________________________________________________________
 | {name.capitalize()} added to Favourites List.                       
@@ -189,17 +216,28 @@ What character would you like to add? (enter full name): """).title().strip()
 
 def remove_character(): 
     """Remove characters from Favourites List"""
-    user_input = input("""
+    name = input("""
 What character would you like to remove? (enter full name): """).title().strip()
-    if user_input in favlist:
-        favlist.pop(user_input)
+    time.sleep(1)
+    if name in favlist:
+        log_df.loc[len(log_df)] = ["remove character", name]
+        favlist.pop(name)
         print(f"""
  _____________________________________________________________________
-| {user_input} successfully removed!                                  |
+| {name} successfully removed!                                  
  _____________________________________________________________________""")
     else:
         print("Character not found! Returning to favourites list...")
     return
+
+def record_actions(action, details=""):
+    global log_df
+    new_row = {
+        "timestamp": datetime.now(),
+        "action": action,
+        "details": details
+    }
+    log_df.loc[len(log_df)] = new_row
 
 #<------------------------------TEST YOUR FUNCTIONS BELOW----------------------------->
 
--- main.py
+++ main.py
@@ -1,30 +1,46 @@
 from functions import *
-import time
-import os
 
 def main():
+    log_df = pd.DataFrame(columns=["ACTION", "USER INPUT"])
     while True:
         print("""
- _________________________
-|   MLP Character Guide   |
-|-------------------------|
-| 1.Search for character  |
-| 2.Filter all characters |
-| 3.View favourites list  |
-| 4.Exit                  |
- _________________________""")
+ _______________________________
+|      MLP Character Guide      |
+|-------------------------------|
+| 1.Search for character        | 
+| 2.Filter all characters       |
+| 3.View favourites list        |
+| 4.View user interactions log  |
+| 5.Exit                        |
+ _______________________________""")
+        time.sleep(1)
         choice = input("""
-Choose an option (1/2/3/4): """).strip()
-
+Choose an option (1/2/3/4/5): """).strip()
+        time.sleep(1)
         if choice == '1':
             name = input("""
 Enter full name of character to search: """).strip()
-            print(search_character(name))
+            time.sleep(1)
+            log_df.loc[len(log_df)] = ["search", name]
+            character = search_character(name)
+            if character:
+                print("")
+                for key, value in character.items():
+                    print(f"\033[1m{key}\033[0m: {value}")
         elif choice == '2':
+            log_df.loc[len(log_df)] = ["filter", ""]
             filter_characters() 
         elif choice == '3':
+            log_df.loc[len(log_df)] = ["view favourites list", ""]
             view_list() 
         elif choice == '4':
+            if log_df.empty:
+                print("""
+User interactions log is empty. Returning to main menu...""")
+            else:
+                print("")
+                print(log_df.to_string(index=False))
+        elif choice == '5':
             print("""
  ________________________________ 
 | Exiting MLP Character Guide... |
@@ -33,8 +49,8 @@ Enter full name of character to search: """).strip()
         else:
             print("""
 Invalid input. Enter a number between 1-4 (1/2/3/4). Please try again""")
-
-
+        time.sleep(2)
+        
 if __name__ == "__main__":
     main()
 
