19f8247b8f42b16479d8ed0428b43fc306baa99d
arisapooeater <arisa.komatsu@education.nsw.gov.au>
2026-03-21 15:35:06 +1100

    fix: log_df  and displaying image
    Debugged log_df and found out it was because the dataframe was being established inside the main loop function so it wasn't properly global to all other functions. I took it outside of the function and also added another row for a timestamp. I fixed the problem I had when testing how to display the image by cutting the URL before using it for image.draw(). I also turned it into a function so the code is much more efficient.

--- functions.py
+++ functions.py
@@ -1,5 +1,5 @@
 import requests # Must import requests so we can use API
-import pandas as pd
+import pandas as pd 
 from datetime import datetime
 from term_image.image import from_url
 import time
@@ -13,40 +13,45 @@ API_URL = "https://ponyapi.net/v1/character/all"
 # Dictionary to store collected favourite characters
 favlist = {}
 
-def search_character(name): #functioning
+def search_character(name): 
     """Search for a MLP character by name and return its details.""" # Triple quotes is a docstring - allows multiline comments!
 
+    # Sends request to retrieve all data from API
     response = requests.get(API_URL)
-
+    
+    # Send error message to user if API is not functioning
     if response.status_code != 200:
         print("API error.")
         return 
 
     characters = response.json()["data"]
 
-    # Search manually
+    # Search manually for data that matches "name" and returns the character's data to the main function
     for c in characters:
         if c["name"] == name.title():
-            return { #change formatting later
+            return { 
                 "Name": c["name"],
                 "Gender": c["sex"],
                 "Kind": ", ".join(c["kind"]) if isinstance(c["kind"], list) else c["kind"],
                 "Occupation": c["occupation"],
                 "Residence": c["residence"],
-                "Image": c["image"][0] if c["image"] else None
+                "Image":  c["image"][0] if c["image"] else None
             }
-    print("Character not found!")
+    print("Character not found! Returning to main menu...") # Display user input error message
     return 
 
-def filter_characters():
+def filter_characters(log_df):
     """Filter MLP characters by sex/kind and display to user."""
 
+    # Sends request to retrieve data from API
     response = requests.get(API_URL)
 
+    # Send error message to user if API is not functioning and retrieval is not possible
     if response.status_code != 200:
         print("API error.")
         return 
     
+    # Retrieve data from API
     characters = response.json()["data"]
 
     print("""
@@ -75,17 +80,21 @@ Filter by (1/2): """).strip()
         if sub_filter == "1":
             print("""
 FEMALE CHARACTERS""")
+            # If characters in the data retrieved from API are female, the character's name is displayed to user
             for c in characters:
                 if c["sex"] == "Female":
                     print(c["name"])
         elif sub_filter == "2":
             print("""
 MALE CHARACTERS""")
+            # If characters in the data retrieved from API are male, the character's name is displayed to user
             for c in characters:
                 if c["sex"] == "Male":
                     print(c["name"])
+        elif sub_filter.lowercase() == 'h':
+            pass
         else:
-            print("Invalid input. Returning to main menu...")
+            print("Invalid input. Returning to main menu...") # Display user input error message
     elif main_filter == "2":
         print("""
  ___________________
@@ -104,60 +113,75 @@ Filter by (1/2/3/4/5): """).strip()
         if sub_filter == "1":
             print("""
 PEGASUS CHARACTERS""")
+            # If characters in the data retrieved from API is a Pegasus, the character's name is displayed to user
             for c in characters:
                 if "Pegasus" in c["kind"]:
                     print(c["name"])
         elif sub_filter == "2":
             print("""
 UNICORN CHARACTERS""")
+            # If characters in the data retrieved from API is a Unicorn, the character's name is displayed to user
             for c in characters:
                 if "Unicorn" in c["kind"]:
                     print(c["name"])
         elif sub_filter == "3":
             print("""
 EARTH PONY CHARACTERS""")
+            # If characters in the data retrieved from API is an Earth Pony, the character's name is displayed to user
             for c in characters:
                 if "Earth" in c["kind"]:
                     print(c["name"])
         elif sub_filter == "4":
             print("""
 ALICORN CHARACTERS""")
+            # If characters in the data retrieved from API is an Alicorn, the character's name is displayed to user
             for c in characters:
                 if "Alicorn" in c["kind"]:
                     print(c["name"])
         elif sub_filter == "5":
             print("""
 OTHER CREATURES""")
+            # If characters in the data retrieved from API is not any of the above kinds, the character's name and kind is displayed to user
             for c in characters:
                 if "Pegasus" not in c["kind"] and "Unicorn" not in c["kind"] and "Earth" not in c["kind"] and "Alicorn" not in c["kind"]:
                     kind = ", ".join(c["kind"]) if isinstance(c["kind"], list) else c["kind"]
                     print(f"{c['name']}: {kind}")
+        elif sub_filter.lowercase() == 'h':
+            pass
         else:
-            print("Invalid input. Enter a number between 1-5 (1/2/3/4/5). Returning to main menu...")
+            print("Invalid input. Enter a number between 1-5 (1/2/3/4/5). Returning to main menu...") # Display user input error message
+        
+        # Record user interaction in log_df
+        record_actions(log_df, "remove character", sub_filter)
+    elif main_filter.lowercase() == 'h':
+        pass
     else:
-        print("Invalid input. Enter a number between 1-2 (1/2). Returning to main menu...")
+        print("Invalid input. Enter a number between 1-2 (1/2). Returning to main menu...") # Display user input error message
     
-def view_list(): 
+def view_list(log_df): 
     """Display all collected MLP characters in Favourites List and ask users if they want to add or remove characters."""
     exit = False
     empty = False
+    # Loop displaying favlist and adding/removing characters until user selects exit Favourites List
     while exit == False:
+        # Check favlist is not empty
         if favlist:
             print("""
 _____________________________________________________
 FAVOURITES LIST""")
-            for name, details in favlist.items():
+            for name, details in favlist.items(): # Display the name, gender, kind, occupation, residence of all characters in favlist
                 empty = False
                 print(f"""
 Name: {details['Name']}
-Sex: {details['Sex']}
+Gender: {details['Gender']}
 Kind: {details['Kind']}
 Occupation: {details['Occupation']}
-Residence: {details['Residence']}
-Image: {details['Image']}""")
-        else:
+Residence: {details['Residence']}""")
+                if details['Image']: # Check there's a value in 'Image' 
+                    display_image(details["Image"]) # Call display_image() function to visualise image in terminal
+        else: # Display empty favlist
             empty = True
-            print("""
+            print(""" 
 _____________________________________________________
 FAVOURITES LIST
                   
@@ -167,7 +191,7 @@ _____________________________________________________""")
         time.sleep(1)
         print("""
  _________________________
-|         OPTIONS         |
+|     FAVLIST OPTIONS     |
 |-------------------------|
 | 1. Add character        |
 | 2. Remove character     |
@@ -178,75 +202,92 @@ _____________________________________________________""")
 Enter action (1/2/3): """).strip()
         time.sleep(1)
         if user_input == "1":
-            add_character()
+            add_character(log_df) # Call add_character() function
         elif user_input == "2":
-            if empty == False:
-                remove_character()
+            if empty == False: # Check favlist wasn't empty
+                remove_character(log_df) # Call remove_character() function
             else:
-                print("Favourites List is empty. Action cannot be taken. Returning to Favourites List...")
+                print("Favourites List is empty. Action cannot be taken. Returning to Favourites List...") # Display error message as there are no characters available to remove from favlist
         elif user_input == "3":
             print("""
  ____________________________
 | Exiting Favourites List... |
  ____________________________""")
-            exit = True
+            exit = True # Break while loop and return to main menu loop
+        elif user_input.lowercase() == 'h':
+            pass
         else:
-            print("Invalid input. Enter a number between 1-3 (1/2/3). Returning to Favourites List...")
+            print("Invalid input. Enter a number between 1-3 (1/2/3). Returning to Favourites List...") # Display user input error message
         time.sleep(2)
 
 
-def add_character(): 
+def add_character(log_df): 
     """Add characters to Favourites List"""
-    global log_df
     name = input("""
 What character would you like to add? (enter full name): """).title().strip()
+    # Get character data that matches name (user input)
     character = search_character(name) 
     time.sleep(1)
+    # Check if character is already in favlist
     if name in favlist:
         print("Character already in Favourites List! Returning to Favourites List...")
+    # Check character exists in API and search_character() returned a value
     elif character:
-        log_df.loc[len(log_df)] = ["add character", name]
+        # Record user interaction in log_df
+        record_actions(log_df,"add character", name)
+
+        # Add character to favlist
         favlist[character["Name"]] = character
         print(f"""
  _____________________________________________________________________
-| {name.capitalize()} added to Favourites List.                       
+| {name.title()} added to Favourites List.                       
  _____________________________________________________________________""")
+    elif name.lowercase() == 'h':
+        pass
     else:
-        print("Invalid input. Character not found")
+        print("Character not found! Returning to Favourites List...") # Display user input error message
     return
     
 
-def remove_character(): 
+def remove_character(log_df): 
     """Remove characters from Favourites List"""
-    global log_df 
     name = input("""
 What character would you like to remove? (enter full name): """).title().strip()
     time.sleep(1)
     if name in favlist:
-        log_df.loc[len(log_df)] = ["remove character", name]
+        # Record user interaction in log_df
+        record_actions(log_df, "remove character", name)
+
+        # Remove character from favlist
         favlist.pop(name)
         print(f"""
  _____________________________________________________________________
-| {name} successfully removed!                                  
+| {name.title()} successfully removed!                                  
  _____________________________________________________________________""")
+    elif name.lowercase() == 'h':
+        pass
     else:
-        print("Character not found! Returning to favourites list...")
+        print("Character not found! Returning to Favourites List...")  # Display user input error message
     return
 
-def record_actions(action, details=""):
-    global log_df
-    new_row = {
-        "timestamp": datetime.now(),
-        "action": action,
-        "details": details
-    }
-    log_df.loc[len(log_df)] = new_row
+def record_actions(log_df, action, details=""):
+    """Record user interactions in a dictionary"""
+    # Add new row to log_df with timestamp, action, and details (user input)
+    log_df.loc[len(log_df)] = [
+        datetime.now(),
+        action,
+        details
+    ]
 
-#<------------------------------TEST YOUR FUNCTIONS BELOW----------------------------->
+def display_image(url):
+    """Display character image in terminal"""
+    # Remove "/revision/latest?cb=..." from image URL as its not compatible with image.draw()
+    fixed_url = url.split("/revision")[0]
 
+    # Create an image instance from a file path
+    image = from_url(fixed_url)
 
-# Create an image instance from a file path
-#image = from_url("https://vignette.wikia.nocookie.net/mlp/images/b/bc/Princess_Twilight_Sparkle_ID_S4E26.png")
+    # Display the image in the terminal
+    image.draw()
+    return
 
-# Display the image in the terminal
-#image.draw()
